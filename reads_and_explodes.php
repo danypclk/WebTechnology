e9dda11dd6c0af8064f2ff933549b93d57c88fb3
danypclk <danypereira264@gmail.com>
2021-12-09 21:07:43 +0100

    experiment

--- reads_and_explodes.php
+++ reads_and_explodes.php
@@ -7,7 +7,6 @@ $fn3 = fopen("client-file.txt","r") or die("fail to open file");
 
 $register_file_name = "register_list.html";
 
-if (file_exists($register_file_name)) {
 	$register_list = fopen($register_file_name, "w");
 	fwrite($register_list,"<h2>Partners</h2>");
 	while($row = fgets($fn)) {
@@ -30,7 +29,6 @@ if (file_exists($register_file_name)) {
 		fwrite($register_list,"<p>Name: ". $Name . ", Password: " . $Pass . ", Email: " . $Email . ", Position: " . $Position . "</p>");
 	}
 	fclose( $fn );
-}
 
 echo "<script>window.location.assign('admin-page.html')</script>";
 ?>
