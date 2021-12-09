5fff06522f8a702cd26099f37660f75e04098ea7
danypclk <danypereira264@gmail.com>
2021-12-09 17:49:11 +0100

    upgrading adminpage services and registry

--- admin.js
+++ admin.js
@@ -1,14 +1,26 @@
 const admin_register = document.getElementById('admin_register');
+const admin_services = document.getElementById('admin_services');
 
 admin_register.addEventListener('click', (e) => {
 	e.preventDefault();
 	
-	displayFunction();
+	displayFunctionregister();
 });
 
-function displayFunction()
+function displayFunctionregister()
 {
+	document.getElementById("services").style.display = "none";
 	document.getElementById("register").style.display = "block";
 }
 
+admin_services.addEventListener('click', (e) => {
+	e.preventDefault();
 	
+	displayFunctionservices();
+});
+
+function displayFunctionservices()
+{
+	document.getElementById("services").style.display = "block";
+	document.getElementById("register").style.display = "none";
+}
---
+++ business.php
@@ -0,0 +1,52 @@
+<?php
+$name = "Client Name: ".$_POST['name']."\n";
+$email = "Client Email: ".$_POST['email']."\n";
+$worker = $_POST['referrer'];
+$task = "Tasks: " .$_POST['notiz']."\n";
+$dienste = "Addresse des Client: " .$_POST['address']."\n";
+$request = "Special requests: " .$_POST['request']."\n";
+
+$worker_1 = "danypereira264@gmail.com";
+$worker_2 = "danysilver300@gmail.com";
+$emadress = "";
+
+if($worker == "r1")
+{
+	$emadress = $worker_1;
+}
+elseif($worker == "r2")
+{
+	$emadress = $worker_2;
+}
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP(); 
+$mail->SMTPDebug = 2;
+$mail->Host = "smtp.gmail.com"; 
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "webtechnologienws21@gmail.com";
+$mail->Password = 'Web21ws21';
+$mail->setFrom('webtechnologienws21@gmail.com', 'Chief Operating Officer');
+$mail->addAddress($emadress);
+$mail->Subject = 'Naechster Auftrag '. date("Y/m/d");
+$mail->msgHTML($name . "<br />" . $email . "<br />" . $task . "<br />" . $dienste . "<br />" . $request);
+$mail->AltBody = 'HTML messaging not supported';
+
+if(!$mail->send()){
+	echo $worker;
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}else{
+    echo "<script>window.location.assign('../admin-files/business.html')</script>";
+}
+?>
