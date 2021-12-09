141116bb0f4d4b3f8c4eecea842eaa181cdedcaa
danypclk <danypereira264@gmail.com>
2021-12-09 18:17:44 +0100

    adding new types of users

--- admin-register.php
+++ admin-register.php
@@ -4,24 +4,96 @@
 	$email = $_POST['email_register'];
 	$jobtitle = $_POST['business_function'];
 	
-	if(file_exists("testfile.txt"))
+	if($jobtitle == "partner")
 	{
-			$myfile = fopen("testfile.txt", "a");
+		if(file_exists("admin-file.txt"))
+		{
+			$myfile = fopen("admin-file.txt", "a");
 			fwrite($myfile, $name . ":");
 			fwrite($myfile, $pass . ":");
 			fwrite($myfile, $email . ":");
-			fwrite($myfile, $jobtitle . "\n");
+			fwrite($myfile, "partner" . "\n");
 			fclose($myfile);
 			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+		else
+		{
+			$myfile = fopen("admin-file.txt", "w");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "partner" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
 	}
-	else
+	else if($jobtitle == "employee")
 	{
-		$myfile = fopen("testfile.txt", "w");
-		fwrite($myfile, $name . ":");
-		fwrite($myfile, $pass . ":");
-		fwrite($myfile, $email . ":");
-		fwrite($myfile, $jobtitle . "\n");
-		fclose($myfile);
-		echo "<script>window.location.assign('admin-page.html')</script>";
+		if(file_exists("employee-file.txt"))
+		{
+			$myfile = fopen("employee-file.txt", "a");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "employee" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+		else
+		{
+			$myfile = fopen("employee-file.txt", "w");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "employee" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+	}
+	else if($jobtitle == "intern")
+	{
+		if(file_exists("intern-file.txt"))
+		{
+			$myfile = fopen("intern-file.txt", "a");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "intern" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+		else
+		{
+			$myfile = fopen("intern-file.txt", "w");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "intern" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+	}
+	else if($jobtitle == "client")
+	{
+		if(file_exists("client-file.txt"))
+		{
+			$myfile = fopen("client-file.txt", "a");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "client" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+		else
+		{
+			$myfile = fopen("client-file.txt", "w");
+			fwrite($myfile, $name . ":");
+			fwrite($myfile, $pass . ":");
+			fwrite($myfile, $email . ":");
+			fwrite($myfile, "client" . "\n");
+			fclose($myfile);
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
 	}
 ?>
