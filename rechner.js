6f36050c50cc92c1f6268b2de63f39304637c01c
Lukas Neuerburg <lukas-neu@t-online.de>
2021-12-10 18:25:04 +0100

    Berechnung hinzugefügt - Reset Button fehlt noch

--- rechner.js
+++ rechner.js
@@ -1,16 +1,33 @@
 "use strict";
 
-let stromkosten = document.querySelector("#stromkosten");
-let betriebszeit = document.querySelector("#betriebszeit");
-let watt = document.querySelector("#watt");
 
-
-
-
-function stromkosten(strompreis, betriebszeit, watt){
-    
-    stromkosten = strompreis / 100  * betriebszeit * watt /1000;
-
-    return stromkosten;
+/* button click - startet Berechnung */ 
+function berechnen(){
+    setErgebnis(stromkosten() + " €");
+}
+/* Setzt den Wert von Ergebnis und gibt ihn aus*/
+function setErgebnis(ergebnis){
+    document.getElementById('ergebnis').innerHTML = ergebnis;
 }
 
+/* bekommt den Wert von Input Feld Strompreis */
+function getStrompreis(){
+    let strompreis = document.getElementById("strompreis").value;
+    return strompreis;
+}
+/* bekommt den Wert von Input Feld Betriebszeit */
+function getBetriebszeit(){
+    let betriebszeit = document.getElementById("betriebszeit").value;
+    return betriebszeit;
+}
+/* bekommt den Wert von Input Feld Watt */
+function getWatt(){
+    let watt = document.getElementById("watt").value;
+    return watt;
+}
+
+/* führt die Berechnung des Ergebnis durch */
+function stromkosten(){
+    stromkosten = getStrompreis() / 100  * getBetriebszeit() * getWatt() /1000;
+    return stromkosten;
+}
