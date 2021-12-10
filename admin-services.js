585ea208854e9375338d81eee9d24b494e8985e3
danypclk <danypereira264@gmail.com>
2021-12-10 13:25:17 +0100

    giving service call form a javascript file

---
+++ admin-services.js
@@ -0,0 +1,59 @@
+const form_services = document.getElementById('form_services');
+const client_name_services = document.getElementById('client_name_services');
+const client_email_services = document.getElementById('client_email_services');
+const client_address_service = document.getElementById('client_address_service');
+
+form_services.addEventListener('submit', (e) => {
+	e.preventDefault();
+	checkservices();
+});
+
+function checkservices() 
+{
+	const client_name_Value = client_name_services.value.trim();
+	const client_email_Value = client_email_services.value.trim();
+	const client_address_Value = client_address_service.value.trim();
+	
+	if(client_name_Value === '')
+	{
+		document.getElementById("client_name_error").style.visibility = "visible";
+	}
+	else
+	{
+		document.getElementById("client_name_error").style.visibility = "hidden";
+	}
+	
+	if(client_email_Value === '')
+	{
+		document.getElementById("client_email_error").style.visibility = "visible";
+	}
+	else
+	{
+		document.getElementById("client_email_error").style.visibility = "hidden";
+	}
+	
+	if(client_address_Value === '')
+	{
+		document.getElementById("client_address_error").style.visibility = "visible";
+	}
+	else
+	{
+		document.getElementById("client_address_error").style.visibility = "hidden";
+	}
+	
+	
+	if(document.querySelectorAll('input[type="radio"][value="Cleaning"]:checked').length === 0 && document.querySelectorAll('input[type="radio"][value="Repair"]:checked').length === 0 && document.querySelectorAll('input[type="radio"][value="Replacement"]:checked').length === 0 && document.querySelectorAll('input[type="radio"][value="Sonstiges"]:checked').length === 0)
+	{
+		document.getElementById("radio_button_error").style.visibility = "visible";
+	}
+	else
+	{
+		document.getElementById("radio_button_error").style.visibility = "hidden";
+	}
+	
+	if(client_name_Value != '' && client_email_Value != '' && client_address_Value != '' &&(document.querySelectorAll('input[type="radio"][value="Cleaning"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Repair"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Replacement"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Sonstiges"]:checked').length === 1))
+	{
+		alert('Successfully tasked an employee!');
+		document.getElementById("form_services").submit();
+	}
+}
