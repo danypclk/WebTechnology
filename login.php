f5338c178b936e6dc80a61186a4856c22d8284b0
danypclk <danypereira264@gmail.com>
2021-12-10 21:08:20 +0100

    better login.php

--- login.php
+++ login.php
@@ -2,16 +2,107 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if($username == 'admin')
+$file = "register-folder/partner-file.txt";
+$file_1 = "register-folder/employee-file.txt";
+$file_2 = "register-folder/intern-file.txt";
+$file_3 = "register-folder/client-file.txt";
+
+if(file_exists($file))
 {
-	if($password == 'loGGin')
-	{
-	echo "<script>alert('Erfolgreich eingeloggt')</script>";
-	echo "<script>window.location.assign('admin-page.html')</script>";
-	}
+	$fn = fopen("register-folder/partner-file.txt","r");
 }
 else
 {
-	echo "<script>window.location.assign('login.html');alert('Falscher Username oder Passwort')</script>";
+	$fn = fopen("register-folder/partner-file.txt", "w");
 }
+
+if(file_exists($file_1))
+{
+	$fn1 = fopen("register-folder/employee-file.txt","r");
+}
+else
+{
+	$fn1 = fopen("register-folder/employee-file.txt", "w");
+}
+
+if(file_exists($file_2))
+{
+	$fn2 = fopen("register-folder/intern-file.txt","r");
+}
+else
+{
+	$fn2 = fopen("register-folder/intern-file.txt", "w");
+}
+
+if(file_exists($file_3))
+{
+	$fn3 = fopen("register-folder/client-file.txt","r");
+}
+else
+{
+	$fn3 = fopen("register-folder/client-file.txt", "w");
+}
+
+	while($row = fgets($fn)) {
+		list( $Name, $Pass, $Email, $Position ) = explode( ":", $row );
+		if($username == $Name)
+		{
+			if($password == $Pass)
+			{
+				echo "<script>window.location.assign('admin-page.html')</script>";
+			}
+		}
+	}
+	fclose($fn);
+
+	while($row = fgets($fn1)) {
+		list( $Name, $Pass, $Email, $Position ) = explode( ":", $row );
+		if($username == $Name)
+		{
+			if($password == $Pass)
+			{
+				echo "<script>window.location.assign('employee-page.html')</script>";
+			}
+		}
+	}
+	fclose($fn1);
+
+	while($row = fgets($fn2)) {
+		list( $Name, $Pass, $Email, $Position ) = explode( ":", $row );
+		if($username == $Name)
+		{
+			if($password == $Pass)
+			{
+				echo "<script>window.location.assign('admin-page.html')</script>";
+			}
+		}
+	}
+	fclose($fn2);
+
+	while($row = fgets($fn3)) {
+		list( $Name, $Pass, $Email, $Position ) = explode( ":", $row );
+		if($username == $Name)
+		{
+			if($password == $Pass)
+			{
+				echo "<script>window.location.assign('admin-page.html')</script>";
+			}
+		}	
+	}
+	fclose($fn3);
+
+	if($username == 'admin')
+	{
+		if($password == 'loGGin')
+		{
+			echo "<script>alert('Erfolgreich eingeloggt')</script>";
+			echo "<script>window.location.assign('admin-page.html')</script>";
+		}
+	}
+	else
+	{
+		echo "<script>alert('Falscher Username oder Passwort')</script>";
+		echo "<script>window.location.assign('login.html')</script>";
+	}
+	
 ?>
