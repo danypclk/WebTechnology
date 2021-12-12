da19b04c6e70ad14f7e7f4ac62ac9b1f6345922d
danypclk <danypereira264@gmail.com>
2021-12-12 16:30:54 +0100

    making selector more responsive

--- admin-services.js
+++ admin-services.js
@@ -2,6 +2,7 @@ const form_services = document.getElementById('form_services');
 const client_name_services = document.getElementById('client_name_services');
 const client_email_services = document.getElementById('client_email_services');
 const client_address_service = document.getElementById('client_address_service');
+const selector = document.getElementById("referrer");
 
 form_services.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -51,7 +52,16 @@ function checkservices()
 		document.getElementById("radio_button_error").style.visibility = "hidden";
 	}
 	
-	if(client_name_Value != '' && client_email_Value != '' && client_address_Value != '' &&(document.querySelectorAll('input[type="radio"][value="Cleaning"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Repair"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Replacement"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Sonstiges"]:checked').length === 1))
+	if(selector.options.length === 0)
+	{
+		document.getElementById("selector_error").style.visibility = "visible";
+	}
+	else
+	{
+		document.getElementById("client_address_error").style.visibility = "hidden";
+	}
+	
+	if(client_name_Value != '' && client_email_Value != '' && client_address_Value != '' && selector.options.length > 0 && (document.querySelectorAll('input[type="radio"][value="Cleaning"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Repair"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Replacement"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Sonstiges"]:checked').length === 1))
 	{
 		alert('Successfully tasked an employee!');
 		document.getElementById("form_services").submit();
