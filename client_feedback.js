25f62dc48d61cd0512dc1a8bfb508506b99c2f6e
danypclk <danypereira264@gmail.com>
2021-12-13 21:07:50 +0100

    client page imporvment

---
+++ client_feedback.js
@@ -0,0 +1,4 @@
+const form_services = document.getElementById('form_feedback');
+const client_name_services = document.getElementById('client_feedback_name');
+const client_email_services = document.getElementById('client_feedback');
+const client_address_service = document.getElementById('client_address_service');
