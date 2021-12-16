d820b7d89a43748836e35e152698e901de096cc0
danypclk <danypereira264@gmail.com>
2021-12-16 22:08:42 +0100

    all three websites are now full responsive

--- admin.js
+++ admin.js
@@ -142,3 +142,31 @@ function displayFunctioncontact_list()
 			document.getElementById("client_messages_list").style.display = "block";
 		}
 }
+
+$(document).scroll(function(){
+          var scrollAmount = $(window).scrollTop();
+          var navHeight = $("nav").height();
+		  var documentWidth = $(document).width();
+		  var calibrater = scrollAmount-navHeight;
+		  if(documentWidth > 1181)
+		  {
+			if(calibrater < 20)
+			{
+				$(".sidebar_menu").css({
+				top: -calibrater + 'px'
+				});
+			}
+			else
+			{
+				$(".sidebar_menu").css({
+				top: 0
+				});
+			}
+		   }
+		   else
+		   {
+			   $(".sidebar_menu").css({
+				top: 120
+				});
+		   }
+      });
--- employee.js
+++ employee.js
@@ -54,3 +54,33 @@ function displayFunctionservices()
 			document.getElementById("order_list").style.display = "none";
 		}
 }
+
+
+$(document).scroll(function(){
+          var scrollAmount = $(window).scrollTop();
+          var navHeight = $("nav").height();
+		  var documentWidth = $(document).width();
+		  var calibrater = scrollAmount-navHeight;
+		  if(documentWidth > 1181)
+		  {
+			if(calibrater < 20)
+			{
+				$(".sidebar_menu").css({
+				top: -calibrater + 'px'
+				});
+			}
+			else
+			{
+				$(".sidebar_menu").css({
+				top: 0
+				});
+			}
+		   }
+		   else
+		   {
+			   $(".sidebar_menu").css({
+				top: 120
+				});
+		   }
+      }); }
+      });
