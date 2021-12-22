ade560bb48f7fad5397ecf589eb6aa94d3b6ae82
danypclk <danypereira264@gmail.com>
2021-12-22 13:26:39 +0100

    changing langugae to german

--- reads_and_explodes_feedback.php
+++ reads_and_explodes_feedback.php
@@ -16,15 +16,15 @@ $feedback_html_location = "iframe-folder/feedback.html";
 $feedback_html_location_worker = "iframe-folder/feedback_worker.html";
 
 	$feedback_file_html_worker = fopen($feedback_html_location_worker, "w");
-	fwrite($feedback_file_html_worker,"<h2>Clients Feedback</h2>");
+	fwrite($feedback_file_html_worker,"<h2>Kunden Feedback</h2>");
 
 	$feedback_file_html = fopen($feedback_html_location, "w");
-	fwrite($feedback_file_html,"<h2>Clients Feedback</h2>");
+	fwrite($feedback_file_html,"<h2>Kunden Feedback</h2>");
 	while($row = fgets($file_feedback)) {
 		list( $Date, $Name, $Feedback, $Person, $Position ) = explode( ":", $row );
 		fwrite($feedback_file_html,"<p>Datum: " . $Date . "</p>");
 		fwrite($feedback_file_html,"<p>Kunde: " . $Name . "</p>");
-		fwrite($feedback_file_html,"<p>Feedback zu: " . $Position  . $Person . "</p>");
+		fwrite($feedback_file_html,"<p>Feedback zu " . $Position  . $Person . "</p>");
 		fwrite($feedback_file_html,"<p>Feedback: " . $Feedback . "</p>");
 		fwrite($feedback_file_html,"<hr />");
 		fwrite($feedback_file_html,"<br />");
@@ -34,7 +34,7 @@ $feedback_html_location_worker = "iframe-folder/feedback_worker.html";
 				$feedback_file_html_worker = fopen($feedback_html_location_worker, "a");
 				fwrite($feedback_file_html_worker,"<p>Datum: " . $Date . "</p>");
 				fwrite($feedback_file_html_worker,"<p>Kunde: " . $Name . "</p>");
-				fwrite($feedback_file_html_worker,"<p>Feedback zu: " . $Position  . $Person . "</p>");
+				fwrite($feedback_file_html_worker,"<p>Feedback zu " . $Position  . $Person . "</p>");
 				fwrite($feedback_file_html_worker,"<p>Feedback: " . $Feedback . "</p>");
 				fwrite($feedback_file_html_worker,"<hr />");
 				fwrite($feedback_file_html_worker,"<br />");
