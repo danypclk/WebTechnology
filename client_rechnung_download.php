b8632a73e3d298d5994d6e6695132239f08b7012
danypclk <danypereira264@gmail.com>
2021-12-23 18:02:15 +0100

    able to download pdf

---
+++ client_rechnung_download.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = $_POST['rechnungen_liste'];
+
+list( $Name, $Invoice_Id ) = explode( ":", $name );
+
+$new_name = trim($Name . "_" . $Invoice_Id);
+
+$file_name =  $new_name . ".pdf";
+$file_url = "Data/Rechnung/" . $file_name;
+header('Content-Type: application/octet-stream');
+header("Content-Transfer-Encoding: Binary"); 
+header("Content-disposition: attachment; filename=\"".$file_name."\""); 
+readfile($file_url);
+exit;
+
+?>
