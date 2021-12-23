aec5dda573712ee7ebc70b1679671724ab84fa23
danypclk <danypereira264@gmail.com>
2021-12-23 14:21:50 +0100

    typos and name changes of task

--- admin-services.js
+++ admin-services.js
@@ -55,8 +55,6 @@ function checkservices()
 		document.getElementById("radio_button_error").style.visibility = "hidden";
 	}
 	
-	console.log(selector_value);
-	
 	if(selector.options.length === 0 || selector_value === 'disabled')
 	{
 		document.getElementById("selector_error").style.visibility = "visible";
@@ -87,7 +85,7 @@ function checkservices()
 		alert('Sie können keinen : im Request Input benutzten!');
 		return false;
 	}
-	else if(client_name_Value != '' && client_email_Value != '' && client_address_Value != '' && selector.options.length > 0 && selector_value != 'disabled' && (document.querySelectorAll('input[type="radio"][value="Cleaning"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Repair"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Replacement"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Sonstiges"]:checked').length === 1))
+	else if(client_name_Value != '' && client_email_Value != '' && client_address_Value != '' && selector.options.length > 0 && selector_value != 'disabled' && (document.querySelectorAll('input[type="radio"][value="Reinigung"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Reparatur"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Ersetzung"]:checked').length === 1 || document.querySelectorAll('input[type="radio"][value="Sonstiges"]:checked').length === 1))
 	{
 		document.getElementById("form_services").submit();
 	}
