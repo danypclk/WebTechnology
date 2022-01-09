42eb25f57a03da6ba1e9e03c659d282f6e5d33e4
Drombie <NoahSimonis2000@gmail.com>
2022-01-09 14:00:03 +0100

    GRundlage Bewerben

---
+++ bewerben.js
@@ -0,0 +1,10 @@
+function randomCode() {
+    let code = Math.random() * 10000;
+    while( code < 1000){
+        code = Math.random() * 1000;
+    }
+
+    document.getElementById("Code").value = Math.round(code);
+
+    
+}
