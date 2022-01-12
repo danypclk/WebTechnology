26c07430c8e07f6b5bd54af80ded9996efc5a678
Marius9953 <marius.graus@web.de>
2022-01-12 17:48:59 +0100

    push

--- admin_rechnung.js
+++ admin_rechnung.js
@@ -2,26 +2,21 @@ const form_rechnung_admin = document.getElementById('form_rechnung_admin');
 const selector_rechnung = document.getElementById("rechnungen_liste_admin");
 
 form_rechnung_admin.addEventListener('submit', (e) => {
-	e.preventDefault();
-	
-	checkRechnung_admin();
+    e.preventDefault();
+
+    checkRechnung_admin();
 });
 
-function checkRechnung_admin() 
-{
+function checkRechnung_admin() {
 
-	if(selector_rechnung.value === '')
-	{
-		document.getElementById("rechnungen_liste_error_admin").style.visibility = "visible";
-		return false;
-	}
-	else
-	{
-		document.getElementById("rechnungen_liste_error_admin").style.visibility = "hidden";
-	}
+    if (selector_rechnung.value === '') {
+        document.getElementById("rechnungen_liste_error_admin").style.visibility = "visible";
+        return false;
+    } else {
+        document.getElementById("rechnungen_liste_error_admin").style.visibility = "hidden";
+    }
 
-	if(selector_rechnung.value != '')
-	{
-		document.getElementById("form_rechnung_admin").submit();
-	}
+    if (selector_rechnung.value != '') {
+        document.getElementById("form_rechnung_admin").submit();
+    }
 }
